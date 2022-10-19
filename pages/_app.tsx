65eed5a61abebdcc50aef62c31ee2b7c0fb2215c
Gerald Olumide <lucignation@gmail.com>
2022-10-19 22:22:52 +0100

    redux toolkit added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
-import "../styles/globals.css"
-import { Layout } from "../components"
-import type { AppProps } from "next/app"
+import { Provider } from 'react-redux';
+import { store, persistor } from '../store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import '../styles/globals.css';
+import { Layout } from '../components';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
-	)
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </PersistGate>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
