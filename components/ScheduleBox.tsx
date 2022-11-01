859faf9b01f958a5fbe7b8965132ae044b39e478
Israeltheminer <israeliyanda5@gmail.com>
2022-11-01 09:21:06 +0100

    Refactor dashboard page & scheduleBox component

--- components/ScheduleBox.tsx
+++ components/ScheduleBox.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import ScheduleItem from "./ScheduleItem"
 
 const ScheduleBox = () => {
 	const [scheduleItems] = useState([
@@ -18,22 +19,20 @@ const ScheduleBox = () => {
 			item: "check in with Principal Smith"
 		}
 	])
+	const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+	const fullDate = new Date()
+	const date = fullDate.getDate()
+	const month = fullDate.getMonth()
+	const year = fullDate.getFullYear()
 	return (
 		<div className='rounded-md shadow-lg p-6 max-w-[380px] bg-white max-h-[212px] overflow-y-auto'>
 			<div className='min-h-[114px]'>
-				<h3 className='text-[20px] font-bold mb-3'>
-					Schedule - <span>Apr 14, 2022</span>
+				<h3 className='text-[20px] font-bold mb-3' data-testid='schedule-box-heading'>
+					Schedule - <span>{`${months[month]} ${date}, ${year}`}</span>
 				</h3>
 				<div className='grid grid-cols-1 divide-y'>
-					{scheduleItems.map(({ id, time, item }) => {
-						return (
-							<div key={id} className='flex align-center px-3 py-1'>
-								<span className='w-[72px] flex'>
-									<p className='text-[#828282] inline-block align-middle my-auto text-sm'>{time}</p>
-								</span>
-								<span className='bg-[#d2c1f5b7] px-4 py-2 rounded-[6px] text-sm w-[235px] h-[35px] truncate ...'>{item}</span>
-							</div>
-						)
+					{scheduleItems?.map(({ id, time, item }) => {
+						return <ScheduleItem key={id} time={time} item={item} id={id} />
 					})}
 				</div>
 			</div>
