daefb9a97851852f2ecbebc02fe5e5994e667946
Israeltheminer <israeliyanda5@gmail.com>
2022-11-03 23:41:12 +0100

    include classlist dropdown and general-nav refactoring

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,21 @@
-import { Provider } from "react-redux";
-import { store, persistor } from "../store/store";
-import { PersistGate } from "redux-persist/integration/react";
-import { Layout } from "../components";
+import { Provider } from "react-redux"
+import { store, persistor } from "../store/store"
+import { PersistGate } from "redux-persist/integration/react"
+import { Layout } from "../components"
 
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import "../styles/globals.css"
+// import "../styles/GeneralNav.css"
+import type { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </PersistGate>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</PersistGate>
+		</Provider>
+	)
 }
-export default MyApp;
+export default MyApp
