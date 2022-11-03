17239f20595a887baf25283cf350289d460a1ba0
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-11-03 04:07:10 -0700

    completed nav dropdown

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,11 +8,32 @@ import { useEffect, useRef, useState } from "react";
 
 export default function Header() {
   const [classTab, setClassTab] = useState<boolean>(false);
-  const [selectedClass, setSelectedClass] = useState<string>('Class C')
+  const [selectedClass, setSelectedClass] = useState<string>('Class C');
+  const [classColor, setClassColor] = useState<string>('#92C7F7');
 
   const toggleClassTab = () => {
     setClassTab(!classTab)
   }
+
+  const classOptions  = [
+    {
+      name: 'Class A',
+      color: '#F6B86F',
+    },
+    {
+      name: 'Class B',
+      color: '#AADE98',
+
+    },
+    {
+      name: 'Class C',
+      color: '#92C7F7',
+    },
+    {
+      name: 'Class D',
+      color: '#FFE977',
+    }
+  ]
 
  
 
@@ -27,43 +48,23 @@ export default function Header() {
       {/* selected hidden at first visible after click */}
       {!classTab && (<div onClick={toggleClassTab} className="absolute w-[14rem] flex justify-between items-center border rounded-full border-[#BDBDBD]  px-3.5 py-2.5">
             <div className="flex items-center justify-between gap-3">
-              <span className="h-8 bg-[#92C7F7] w-8 rounded-full"></span>
+              <span className="h-8  w-8 rounded-full" style={{background: classColor}}></span>
               <h5 className="font-bold">{selectedClass}</h5>
             </div>
             <FaChevronDown  className="text-[#838383]"/>
         </div>)}
         
 
-        {classTab && (<div onClick={toggleClassTab} className="border-[#BDBDBD] border rounded-[1.6rem] mt-[10rem] bg-white z-30">
-          <div className="w-[14rem] flex justify-between items-center border-b-2  px-3.5 py-2.5">
+        {classTab && (
+        <div onClick={toggleClassTab} className="border-[#BDBDBD] border rounded-[1.6rem] mt-[10rem] bg-white z-30">
+          {classOptions.map((option) => {
+          return (<div className="w-[14rem] flex justify-between items-center border-b-2  px-3.5 py-2.5" onClick={() => {setSelectedClass(option.name), setClassColor(option.color)}}>
             <div className="flex items-center justify-between gap-3">
-              <span className="h-8 bg-[#92C7F7] w-8 rounded-full"></span>
-              <h5 className="font-bold">Class C</h5>
+              <span className="h-8 w-8 rounded-full" style={{background: option.color}}></span>
+              <h5 className="font-bold">{option.name}</h5>
             </div>
-            <FaChevronUp className=" text-[#838383]" />
-          </div>
-          <div className="w-[14rem] flex justify-between items-center border-b-2  px-3.5 py-2.5">
-            <div className="flex items-center justify-between gap-3">
-              <span className="h-8 bg-[#92C7F7] w-8 rounded-full"></span>
-              <h5 className="font-bold">Class A</h5>
-            </div>
-            
-          </div>
-          <div className="w-[14rem] flex justify-between items-center border-b-2  px-3.5 py-2.5">
-            <div className="flex items-center justify-between gap-3">
-              <span className="h-8 bg-[#92C7F7] w-8 rounded-full"></span>
-              <h5 className="font-bold">Class B</h5>
-            </div>
-            
-          </div>
-          <div className="w-[14rem] flex justify-between items-center px-3.5 py-2.5">
-            <div className="flex items-center justify-between gap-3">
-              <span className="h-8 bg-[#92C7F7] w-8 rounded-full"></span>
-              <h5 className="font-bold">Class D</h5>
-            </div>
-         
-          </div>
-       
+            {option.name == 'Class A' ? <FaChevronUp className=" text-[#838383]" /> : ''}
+          </div>)})}
       </div>)}
       </div>
       {/* avatar info */}
