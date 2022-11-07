6be23ed7fba1054ac1ed7458bbfe66c606f21783
Israeltheminer <israeliyanda5@gmail.com>
2022-11-07 22:19:48 +0100

    Rearrange curriculum file-structure, add css animations, and Button size and color change

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,8 +2,8 @@ import React from "react"
 
 const Button = ({ text, color }: { text: string; color: string }) => {
 	return (
-		<button type='button' style={{ backgroundColor: color }} className='w-[180px] font-bold h-[44px] text-white rounded-3xl'>
-			{text}
+		<button type='button' style={ { backgroundColor: color } } className='min-w-[130px] font-bold h-[44px] text-white rounded-3xl px-8 hover:opacity-80'>
+			{ text }
 		</button>
 	)
 }
