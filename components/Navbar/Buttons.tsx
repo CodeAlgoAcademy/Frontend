cfac11b3c130c705fce805f58c5ff8df4aad3594
daniel-dunsin <adejaredaniel12@gmail.com>
2022-11-08 16:48:21 +0100

    updated ui

--- components/Navbar/Buttons.tsx
+++ components/Navbar/Buttons.tsx
@@ -1,32 +1,33 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const styles = {
-    lgBtn: 'uppercase p-3 text-white hover:bg-opacity-50 rounded'
-}
+  lgBtn: "uppercase p-3 text-white hover:bg-opacity-50 rounded",
+};
 
 const Buttons = () => {
-    return (
-        <>
-            <div className='hidden md:flex space-x-2'>
-                <button className={`bg-orange-500 ${styles.lgBtn}`}>
-                    Login
-                </button>
-                <button className={`bg-amber-500 ${styles.lgBtn}`}>
-                    Signup
-                </button>
-            </div>
+  return (
+    <>
+      <div className="hidden md:flex space-x-2">
+        <Link href="/login">
+          <button className={`bg-orange-500 ${styles.lgBtn}`}>Login</button>
+        </Link>
+        <Link href="signup">
+          <button className={`bg-amber-500 ${styles.lgBtn}`}>Signup</button>
+        </Link>
+      </div>
 
-            {/* mobile devices */}
-            <div className='grid gap-3 md:hidden'>
-                <button className={`bg-orange-500 ${styles.lgBtn}`}>
-                    Login
-                </button>
-                <button className={`bg-amber-500 ${styles.lgBtn}`}>
-                    Signup
-                </button>
-            </div>
-        </>
-    )
-}
+      {/* mobile devices */}
+      <div className="grid gap-3 md:hidden">
+        <Link href="/login">
+          <button className={`bg-orange-500 ${styles.lgBtn}`}>Login</button>
+        </Link>
+        <Link href="/signup">
+          <button className={`bg-amber-500 ${styles.lgBtn}`}>Signup</button>
+        </Link>
+      </div>
+    </>
+  );
+};
 
-export default Buttons
+export default Buttons;
