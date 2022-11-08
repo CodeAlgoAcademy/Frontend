00fc22f4b5639538cb040bc42eaec471464c19c9
Israeltheminer <israeliyanda5@gmail.com>
2022-11-08 22:33:00 +0100

    add modal animation, data collection components & general page state

--- pages/curriculum/assignments.tsx
+++ pages/curriculum/assignments.tsx
@@ -1,25 +1,34 @@
 import React, { useState } from "react"
 import { Button, Sidebar, GeneralNav } from "../../components"
 import { BsPlusCircle } from "react-icons/bs"
-import { RiArrowDropDownLine } from "react-icons/ri"
 import Link from "next/link"
+import { FaTimes } from "react-icons/fa"
+import { IoPersonAddOutline } from "react-icons/io5"
+import { TbMedal } from "react-icons/tb"
 
 const Assignments = () => {
    const modalDefaults = {
       saveResponse: false,
       createResponse: false,
       cancelResponse: false,
-      historyResponse: false
+      historyResponse: false,
+      skillsResponse: false,
+      studentResponse: false
    }
    const [modalWrapperDisplay, setModalWrapperDisplay] = useState(false)
+   const [skills, setSkills] = useState({ currentValue: 2 })
+   const [questionsRange, setQuestionsRange] = useState({ currentValue: "0" })
    const [modalItemsDisplay, setModalItemsDisplay] = useState(modalDefaults)
    const showModal = (modalName: string) => {
       setModalWrapperDisplay((prev) => true)
       setModalItemsDisplay((prev) => prev = ({ ...prev, [modalName]: true }))
    }
    const hideModal = () => {
-      setModalItemsDisplay((prev) => (modalDefaults))
       setModalWrapperDisplay((prev) => false)
+      setTimeout(() => setModalItemsDisplay((prev) => (modalDefaults)), 1000)
+   }
+   const switchModal = (modalName: string) => {
+      setModalItemsDisplay((prev) => prev = ({ ...modalDefaults, [modalName]: true }))
    }
    return (
       <div className='min-h-[100vh] flex flex-col'>
@@ -48,57 +57,25 @@ const Assignments = () => {
                      <div className='rounded-md h-[58px] bg-white flex items-center overflow-y-hidden'>
                         <div className='rounded-r-md flex items-center justify-between px-4 w-[180px] shadow-right h-full'>
                            <span className='font-bold'>Skill(s)</span>
-                           <div className='text-[32px]'>
-                              <RiArrowDropDownLine />
-                           </div>
+                           <span className="text-2xl cursor-pointer hover:opacity-80 hover:scale-125 transition-all ease-in-out animate-pulse hover:animate-none"><TbMedal /> </span>
                         </div>
                         <div className='px-14'>
-                           <span className="font-medium">0 skill(s) selected</span>
+                           <span className="font-medium">{ skills?.currentValue } skill(s) selected</span>
                         </div>
                      </div>
                      <div className='rounded-md h-[58px] bg-white flex items-center overflow-y-hidden'>
                         <div className='rounded-r-md flex items-center justify-between px-4 w-[180px] shadow-right h-full'>
                            <span className='font-bold'>Student(s)</span>
-                           <div className='text-[32px]'>
-                              <RiArrowDropDownLine />
-                           </div>
+                           <span className="text-2xl cursor-pointer hover:opacity-80 hover:scale-125 transition-all ease-in-out animate-pulse hover:animate-none"><IoPersonAddOutline /> </span>
                         </div>
                         <div className='px-14'>
-                           <span className="font-medium">Selected Students</span>
+                           <span className="font-medium">0 student(s) selected</span>
                         </div>
                      </div>
-                     <div className='rounded-md h-[58px] bg-white flex items-center overflow-y-hidden'>
-                        <div className='rounded-r-md flex items-center justify-between px-4 w-[180px] shadow-right h-full'>
-                           <span className='font-bold'>Parameters</span>
-                           <div className='text-[32px]'>
-                              <RiArrowDropDownLine />
-                           </div>
-                        </div>
-                        <div className='px-14 flex items-center gap-8'>
-                           <span className="font-medium">Order of questions</span>
-                           <div className="flex items-center gap-14">
-                              <div className="form-check flex items-center gap-2">
-                                 <input
-                                    className="" type="radio" name="arrangementRadio" id="arrangementRadio1" />
-                                 <label className="form-check-label inline-block text-gray-800 font-medium" htmlFor="arrangementRadio1">
-                                    Random
-                                 </label>
-                              </div>
-                              <div className="form-check flex items-center gap-2">
-                                 <input className="" type="radio" name="arrangementRadio" id="arrangementRadio2" checked />
-                                 <label className="form-check-label inline-block text-gray-800 font-medium" htmlFor="arrangementRadio2">
-                                    Sequence
-                                 </label>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
+
                      <div className='rounded-md h-[58px] bg-white flex items-center overflow-y-hidden'>
                         <div className='rounded-r-md flex items-center justify-between px-4 w-[180px] shadow-right h-full'>
                            <span className='font-bold'>Scheduling</span>
-                           <div className='text-[32px]'>
-                              <RiArrowDropDownLine />
-                           </div>
                         </div>
                         <div className='px-14'>
                            <div className="flex items-center gap-14">
@@ -113,6 +90,41 @@ const Assignments = () => {
                                  <input className="" type="radio" name="scheduleRadio" id="scheduleRadio2" checked />
                                  <label className="form-check-label inline-block text-gray-800 font-medium" htmlFor="scheduleRadio2">
                                     Schedule for later date
+                                 </label>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     <div className='rounded-md h-[58px] bg-white flex items-center overflow-y-hidden'>
+                        <div className='rounded-r-md flex items-center justify-between px-4 w-[180px] shadow-right h-full'>
+                           <span className='font-bold'>No. of questions</span>
+                        </div>
+                        <div className='px-14 flex gap-8 items-center'>
+                           <input id="questionCount" type="number" value={ questionsRange?.currentValue } className="bg-gray-100 px-4 py-2 max-w-[100px] assignment-input caret-transparent font-medium outline-none border-none rounded-md" />
+                           <div>
+                              <input type="range" min={ 0 } max={ 100 } value={ questionsRange?.currentValue } step={ skills?.currentValue } className="w-[380px] h-[12px] appearance-none rounded-lg bg-gray-200 opacity-90 outline-none transition-all hover:opacity-100 assignment-slider" onChange={ (e) => {
+                                 setQuestionsRange((prev) => ({ currentValue: e.target.value }))
+                              } } />
+                           </div>
+                        </div>
+                     </div>
+                     <div className='rounded-md h-[58px] bg-white flex items-center overflow-y-hidden'>
+                        <div className='rounded-r-md flex items-center justify-between px-4 w-[180px] shadow-right h-full'>
+                           <span className='font-bold'>Order of questions</span>
+                        </div>
+                        <div className='px-14 flex items-center gap-8'>
+                           <div className="flex items-center gap-14">
+                              <div className="form-check flex items-center gap-2">
+                                 <input
+                                    className="" type="radio" name="arrangementRadio" id="arrangementRadio1" />
+                                 <label className="form-check-label inline-block text-gray-800 font-medium" htmlFor="arrangementRadio1">
+                                    Random
+                                 </label>
+                              </div>
+                              <div className="form-check flex items-center gap-2">
+                                 <input className="" type="radio" name="arrangementRadio" id="arrangementRadio2" checked />
+                                 <label className="form-check-label inline-block text-gray-800 font-medium" htmlFor="arrangementRadio2">
+                                    Sequence
                                  </label>
                               </div>
                            </div>
@@ -145,17 +157,24 @@ const Assignments = () => {
             <div className={ `w-full h-full ${modalWrapperDisplay ? "showModal" : "hideModal"} backdrop-blur-sm bg-gray-100/50 fixed left-0 z-50 flex justify-center items-center` }>
                {
                   modalWrapperDisplay &&
-                  <div className="">
+                  <div className="relative">
+                     <span className="text-2xl absolute right-8 top-8 cursor-pointer hover:scale-110 hover:opacity-90 transition-all ease-in-out" onClick={ hideModal }>
+                        <FaTimes />
+                     </span>
                      { modalItemsDisplay?.saveResponse &&
                         <div className="px-24 py-20 rounded-xl bg-white font-semibold text-center text-xl">
                            <p>You have successfully saved an assignment</p>
-                           <p>Click on <span className="font-bold text-[#F28E2C] cursor-pointer">ASSINGMENT HISTORY</span> to view your Assingment.</p>
+                           <p>Click on <span className="font-bold text-[#F28E2C] cursor-pointer" onClick={ () => {
+                              switchModal("assignmentHistory")
+                           } }>ASSINGMENT HISTORY</span> to view your Assingment.</p>
                         </div>
                      }
                      { modalItemsDisplay?.createResponse &&
                         <div className="px-24 py-20 rounded-xl bg-white font-semibold text-center text-xl">
                            <p>You have successfully created an assignment</p>
-                           <p>Click on <span className="font-bold text-[#F28E2C] cursor-pointer">ASSINGMENT HISTORY</span> to view your Assingment.</p>
+                           <p>Click on <span className="font-bold text-[#F28E2C] cursor-pointer" onClick={ () => {
+                              switchModal("assignmentHistory")
+                           } }>ASSINGMENT HISTORY</span> to view your Assingment.</p>
                         </div>
                      }
                      { modalItemsDisplay?.cancelResponse &&
