d99ab57ba406a732edbdf3617b6787421db554d6
Israeltheminer <israeliyanda5@gmail.com>
2022-11-24 21:58:39 +0100

    calender structure and styling

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,63 +1,66 @@
-import React, { useState } from "react"
-import {
-   ScheduleComponent,
-   ViewsDirective,
-   ViewDirective,
-   Day,
-   Week,
-   WorkWeek,
-   Month,
-   Agenda,
-   Inject,
-   Resize,
-} from "@syncfusion/ej2-react-schedule"
-import { DatePickerComponent } from "@syncfusion/ej2-react-calendars"
+import React, { useState, PropsWithChildren } from "react"
+import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, EventRenderedArgs } from "@syncfusion/ej2-react-schedule"
+import { DatePickerComponent, ChangeEventArgs } from "@syncfusion/ej2-react-calendars"
+import { Sidebar, GeneralNav } from "../components"
+import { useSelector } from "react-redux"
+import { RootState } from "../store/store"
 
-import { Header } from "../components/calendar"
-
-// eslint-disable-next-line react/destructuring-assignment
-const PropertyPane = (props) => <div className='mt-5'>{ props.children }</div>
+const PropertyPane = (props: PropsWithChildren) => <div className='mt-5'>{ props.children }</div>
 
 const Calendar = () => {
-   const [scheduleObj, setScheduleObj] = useState()
-
-   const change = (args) => {
+   let scheduleObj: ScheduleComponent | any
+   const scheduleData = useSelector((state: RootState) => state.calendar)
+   function change (args: ChangeEventArgs): void {
       scheduleObj.selectedDate = args.value
       scheduleObj.dataBind()
    }
 
    return (
-      <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
-         <Header title='Calendar' />
-         <ScheduleComponent
-            height='650px'
-            ref={ (schedule) => setScheduleObj(schedule) }
-            selectedDate={ new Date(2021, 0, 10) }
-            eventSettings={ { dataSource: scheduleData } }>
-            <ViewsDirective>
-               { ["Day", "Week", "WorkWeek", "Month", "Agenda"].map((item) => (
-                  <ViewDirective key={ item } option={ item } />
-               )) }
-            </ViewsDirective>
-            <Inject services={ [Day, Week, WorkWeek, Month, Agenda, Resize] } />
-         </ScheduleComponent>
-         <PropertyPane>
-            <table style={ { width: "100%", background: "white" } }>
-               <tbody>
-                  <tr style={ { height: "50px" } }>
-                     <td style={ { width: "100%" } }>
-                        <DatePickerComponent
-                           value={ new Date(2022, 10, 21) }
-                           showClearButton={ false }
-                           placeholder='Current Date'
-                           floatLabelType='Always'
-                           change={ change }
-                        />
-                     </td>
-                  </tr>
-               </tbody>
-            </table>
-         </PropertyPane>
+      <div className="min-h-[100vh]">
+         <GeneralNav />
+         {/* <Header /> */ }
+         <div className="flex mb-auto relative">
+            <div className="sidebar bg-white w-[270px] relative">
+               <div className="sticky top-0 left-0">
+                  <Sidebar />
+               </div>
+            </div>
+            <div className="bg-[#E5E5E5] flex-1 px-[2%] py-8">
+               <div className='mt-6 p-4 bg-white rounded-xl max-w-[1200px] mx-auto'>
+                  <ScheduleComponent
+                     height='650px'
+                     selectedDate={ new Date() }
+                     ref={ schedule => scheduleObj = schedule }
+                     eventSettings={ { dataSource: scheduleData } }>
+                     <ViewsDirective>
+                        <ViewDirective option='Day' />
+                        <ViewDirective option='Week' />
+                        <ViewDirective option='WorkWeek' />
+                        <ViewDirective option='Month' />
+                        <ViewDirective option='Agenda' />
+                     </ViewsDirective>
+                     <Inject services={ [Day, Week, WorkWeek, Month, Agenda, Resize] } />
+                  </ScheduleComponent>
+                  <PropertyPane>
+                     <table style={ { width: "100%", background: "white" } }>
+                        <tbody>
+                           <tr style={ { height: "50px" } }>
+                              <td style={ { width: "100%" } }>
+                                 <DatePickerComponent
+                                    value={ new Date() }
+                                    showClearButton={ false }
+                                    placeholder='Current Date'
+                                    floatLabelType='Always'
+                                    change={ change.bind(this) }
+                                 />
+                              </td>
+                           </tr>
+                        </tbody>
+                     </table>
+                  </PropertyPane>
+               </div>
+            </div>
+         </div>
       </div>
    )
 }
