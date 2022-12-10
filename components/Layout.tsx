890c71348b352745b18f525729c974baa3fe4db8
daniel-dunsin <adejaredaniel12@gmail.com>
2022-12-10 22:27:36 +0100

    added env for google id

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -36,7 +36,9 @@ const Layout = ({ children }: Props) => {
     }
   }, []);
   return (
-    <GoogleOAuthProvider clientId="1015154836917-ria2t0r69q3jgc9td4cqb76h7lmnkann.apps.googleusercontent.com">
+    <GoogleOAuthProvider
+      clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string}
+    >
       <div>
         <Head>
           <title>CodeAlgo Academy</title>
