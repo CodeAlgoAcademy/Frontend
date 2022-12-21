0a2aae1727724b68ca116dfaca8b76237036876b
Israeltheminer <israeliyanda5@gmail.com>
2022-12-21 14:58:30 +0100

    Fix dashboard schedule and current lesson display

--- components/ScheduleBox.tsx
+++ components/ScheduleBox.tsx
@@ -1,39 +1,59 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import ScheduleItem from "./ScheduleItem"
+import { getSchedule } from "services/scheduleService"
+import { useSelector, useDispatch } from "react-redux"
+import { AppDispatch, RootState } from "../store/store"
+import { Schedule } from "types/interfaces"
 
 const ScheduleBox = () => {
-	const [scheduleItems] = useState([
-		{
-			id: 1,
-			time: "9:30 am",
-			item: "tell class about guest speaker"
-		},
-		{
-			id: 2,
-			time: "11:15 am",
-			item: "reminder about presentation"
-		},
-		{
-			id: 3,
-			time: "2:15 pm",
-			item: "check in with Principal Smith"
+	const scheduleData: Schedule = useSelector((state: RootState) => state.schedule)
+	const [currentSchedule, setCurrentSchedule] = useState([])
+	const [allSchedule] = useState(scheduleData.allSchedule)
+	const dispatch = useDispatch<AppDispatch>()
+	function isToday (date: Date): boolean {
+		const today = new Date()
+		if (today.toDateString() === date.toDateString()) {
+			return true
 		}
-	])
+		return false
+	}
+	function formatAMPM (date: Date) {
+		let hours = date.getHours()
+		let minutes: any = date.getMinutes()
+		let ampm = hours >= 12 ? 'pm' : 'am'
+		hours = hours % 12
+		hours = hours ? hours : 12 // the hour '0' should be '12'
+		minutes = minutes < 10 ? '0' + minutes : minutes
+		let strTime = hours + ':' + minutes + ' ' + ampm
+		return strTime
+	}
+	const fetchSchedule = async () => {
+		const data = await dispatch(getSchedule())
+	}
 	const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 	const fullDate = new Date()
 	const date = fullDate.getDate()
 	const month = fullDate.getMonth()
 	const year = fullDate.getFullYear()
+	useEffect(() => {
+		fetchSchedule()
+	}, [])
+	useEffect(() => {
+		const data = allSchedule.filter((item: any) => isToday(item.StartTime))
+		setCurrentSchedule((prev) => data)
+	}, [allSchedule])
 	return (
 		<div className='rounded-md shadow-lg p-6 max-w-[380px] bg-white max-h-[212px] overflow-y-auto'>
 			<div className='min-h-[114px]'>
 				<h3 className='text-[20px] font-bold mb-3' data-testid='schedule-box-heading'>
-					Schedule - <span>{`${months[month]} ${date}, ${year}`}</span>
+					Schedule - <span>{ `${months[month]} ${date}, ${year}` }</span>
 				</h3>
 				<div className='grid grid-cols-1 divide-y'>
-					{scheduleItems?.map(({ id, time, item }) => {
-						return <ScheduleItem key={id} time={time} item={item} id={id} />
-					})}
+					{ currentSchedule.length > 0 && (
+						currentSchedule?.map(({ Id, StartTime, Subject }) => {
+							return <ScheduleItem key={ Id } time={ formatAMPM(StartTime) } item={ Subject } id={ Id } />
+						}))
+					}
 				</div>
 			</div>
 		</div>
