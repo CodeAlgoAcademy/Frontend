07d66f069b37106677500b9876040161f96aa8c4
daniel-dunsin <adejaredaniel12@gmail.com>
2022-12-31 12:05:32 +0100

    fixed pipeline

--- components/Teachers/students/StudentTable.tsx
+++ components/Teachers/students/StudentTable.tsx
@@ -29,13 +29,13 @@ const StudentTable = ({ details }: { details: object[] }) => {
             </td>
             <td>
               <td className={styles.assignment}>
-                <LinearProgress
+                {/* <LinearProgress
                   variant="determinate"
                   value={+course.title}
                   sx={{ borderRadius: 5 }}
                   color="success"
-                />
-                <p>{course.title}%</p>
+                /> */}
+                <p>{course.title}</p>
               </td>
             </td>
 
@@ -53,9 +53,10 @@ export default StudentTable;
 const styles = {
   table: "table-auto w-full",
   headingRow: "p-4 text-left text-sm",
-  headingColumn: "font-medium p-4",
+  headingColumn: "font-medium p-4 ",
   bodyRow: "even:bg-slate-100 odd:bg-white p-2  overflow-x-scroll",
   bodyColumn: "py-3 px-2 text-xs capitalize",
   progress: "py-3 px-2 text-[11px] grid grid-cols-2 items-center space-x-3",
-  assignment: "py-3 px-2 text-[11px] grid grid-cols-2 items-center space-x-3",
+  assignment:
+    "py-3 px-2 text-[11px] grid grid-cols-2 items-center space-x-3 text-center",
 };
