97e5f857f3859569478fd4a8d287341e9996866b
Olamide Simon <simonadepetoye@gmail.com>
2022-12-31 16:30:26 +0100

    Delete schedule fixed and Websocket setup

--- services/scheduleService.ts
+++ services/scheduleService.ts
@@ -9,6 +9,7 @@ export const getSchedule = createAsyncThunk("scheduleSlice/getSchedule", async (
 				Authorization: `Bearer ${getAccessToken()}`
 			}
 		})
+		console.log(data)
 		return data
 	} catch (error: any) {
 		return thunkApi.rejectWithValue(error.response.data)
@@ -30,12 +31,22 @@ export const postSchedule = createAsyncThunk("scheduleSlice/postSchedule", async
 })
 
 export const putSchedule = createAsyncThunk("scheduleSlice/putSchedule", async (updatedRecords: any, thunkApi) => {
-	const changedRecord = updatedRecords[0]
-	console.log(changedRecord);
+	for(var i = 0; i < updatedRecords.length; i++) {
+		var a = updatedRecords[i];
+
+		for (var key in a) {
+			if (a.hasOwnProperty(key)) {
+				a[key.charAt(0).toLowerCase() + key.substring(1)] = a[key];
+				delete a[key];
+			}
+		}
+		updatedRecords[i] = a
+	}
+
 	try {
 		const { data } = await http.put(
-			`/academics/calendar/schedules/${changedRecord.Id}`,
-			{ ...changedRecord },
+			`/academics/calendar/schedules/${updatedRecords[0].Id}`,
+			{ ...updatedRecords },
 			{
 				headers: {
 					Authorization: `Bearer ${getAccessToken()}`
@@ -50,11 +61,21 @@ export const putSchedule = createAsyncThunk("scheduleSlice/putSchedule", async (
 
 export const deleteSchedule = createAsyncThunk("scheduleSlice/deleteSchedule", async (deletedRecords: any, thunkApi) => {
 	try {
+		for(var i = 0; i < deletedRecords.length; i++) {
+			var a = deletedRecords[i];
+
+			for (var key in a) {
+				if (a.hasOwnProperty(key) && key !== 'Subject') {
+					a[key.charAt(0).toLowerCase() + key.substring(1)] = a[key];
+					delete a[key];
+				}
+			}
+			deletedRecords[i] = a
+		}
 		console.log(deletedRecords)
+
 		const { data } = await http.delete("/academics/calendar/schedules/delete/", {
-			data: {
-				source: deletedRecords
-			},
+			data: JSON.stringify(deletedRecords),
 			headers: {
 				Authorization: `Bearer ${getAccessToken()}`
 			}
