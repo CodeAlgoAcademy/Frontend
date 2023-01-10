b5a01f537d2ee2af1359d733b673cbb9cb747b31
Olamide Simon <simonadepetoye@gmail.com>
2023-01-10 22:19:42 +0100

    Hey

---
+++ types/interfaces/notifications.ts
@@ -0,0 +1,10 @@
+export type NotificationTypes = 'success' | 'error' | 'info'
+
+export type Notification = {
+  id: number,
+  message: string
+  autoHideDuration: number
+  type: NotificationTypes
+  direction: string,
+  position: string
+}
