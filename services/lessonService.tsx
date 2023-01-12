764dfd47007c2c64d729086cbabf0a29c93a0173
Adejare Daniel <adejaredaniel12@gmail.com>
2023-01-12 16:36:19 +0000

    Update lessonService.tsx

--- services/lessonService.tsx
+++ services/lessonService.tsx
@@ -7,18 +7,18 @@ export const getAllLessons: any = createAsyncThunk(
   'lessonSlice/getLessons',
   async (topics, thunkApi) => {
     const dispatch = thunkApi.dispatch;
-    dispatch(openPreloader({ loadingText: 'Fetching Lessons' }));
+    
     try {
       const { data } = await http.get(`/academics/curriculums/lessons/?unit=${topics}`, {
         headers: { Authorization: 'Bearer ' + getAccessToken() },
       });
-      dispatch(closePreloader());
+      
       return data;
     } catch (error: any) {
       if (error.response.status !== 401) {
         dispatch(openErrorModal({ errorText: [error.message] }));
       }
-      dispatch(closePreloader());
+      
       return thunkApi.rejectWithValue(error.response.data);
     }
   },
