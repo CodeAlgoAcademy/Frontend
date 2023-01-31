e2d91bad284c1e80d64d328055fae6218afcc4ed
israeltheminer <israeliyanda5@gmail.com>
2023-01-31 15:59:03 +0100

    include changes for smaller devices

--- components/parents/NavButton.tsx
+++ components/parents/NavButton.tsx
@@ -8,9 +8,11 @@ interface Props {
    title: string;
    notification?: number;
    name: string;
+   isIcon?: boolean;
 }
-const NavButton = ({image,title,notification,name}: Props) => {
+const NavButton = ({image,title,notification,name,isIcon}: Props) => {
    const [active,setActive] = useState(false);
+   const [detailsDisplay,setDetailsDisplay] = useState(false);
    const router = useRouter();
    console.log(router.pathname);
    useEffect(() => {
@@ -19,11 +21,21 @@ const NavButton = ({image,title,notification,name}: Props) => {
 
    return (
       <Link href={`/parents/${name}`}>
-         <button className='py-[14px] px-7 flex items-center gap-5 w-full hover:bg-slate-50 rounded-3xl' style={{backgroundColor: active ? "#2073fa" : ""}}>
+         <button className='py-[14px] px-7 flex relative items-center gap-5 w-full hover:bg-slate-50 rounded-3xl' style={{backgroundColor: active ? "#2073fa" : "",padding: !isIcon ? "14px 28px" : "12px",borderRadius: !isIcon ? "24px" : "6px"}} onMouseEnter={() => setDetailsDisplay(() => true)} onMouseLeave={() => setDetailsDisplay(() => false)}>
             <Image src={`/assets/${image}`} alt={title} width={20} height={20} className={active ? "" : "blue-svg"} />
-            <p className='font-medium select-none' style={{color: active ? "#fff" : ""}}>{title}</p>
-            {notification &&
-               <span className='w-5 h-5 rounded-full text-sm text-white bg-[#FB4DAB]'>{`${notification}`}</span>
+            {
+               !isIcon &&
+               <>
+                  <p className='font-medium select-none' style={{color: active ? "#fff" : ""}}>{title}</p>
+                  {notification &&
+                     <span className='w-5 h-5 rounded-full text-sm text-white bg-[#FB4DAB]'>{`${notification}`}</span>
+                  }
+               </>
+            }
+            {
+               isIcon && detailsDisplay && <div className=' bg-white border border-slate-200 min-w-[80px] text-xs font-medium py-2 px-4 rounded-xl absolute top-0 left-[120%]'>
+                  {title}
+               </div>
             }
          </button>
       </Link>
