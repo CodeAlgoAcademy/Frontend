8805a8116f0c27d81ecf4bc40bf295aa1a03731a
israeltheminer <israeliyanda5@gmail.com>
2023-02-04 00:25:17 +0100

    content box and general parent layout fixes

--- pages/parents/screen-time.tsx
+++ pages/parents/screen-time.tsx
@@ -7,9 +7,9 @@ import BarChart from '@/components/parents/BarChart';
 const ScreenTime = () => {
    return (
       <ParentLayout>
-         <div className='flex flex-col gap-9'>
+         <div className='overflow-x-auto flex flex-col gap-9 mx-4 sm:mx-0'>
             <ContentBox size="large" title='Screen Time' padding='large'>
-               <BarChart data={[2.4,3.1,4,3.9,3.5,2.9,3]} barSpace={9.6} barWidth={3.3} maxHours={4} />
+               <BarChart data={[2.8,6.5,4,5,3.5,7,3]} barSpace={9.6} barWidth={3.3} maxHours={8} />
             </ContentBox>
             <ContentBox size="large" title='Current screen time restrictions' padding='large'></ContentBox>
          </div>
