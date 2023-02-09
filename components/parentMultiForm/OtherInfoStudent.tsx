7ca046601b34a8c55b97fcdc3d1f78c0fec04178
Emmanuel Akinsiku <akinsnuel@gmail.com>
2023-02-09 09:05:42 -0800

    student signup completed

--- components/parentMultiForm/OtherInfoStudent.tsx
+++ components/parentMultiForm/OtherInfoStudent.tsx
@@ -1,21 +1,31 @@
 import React, { ChangeEvent, ReactNode } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateUser } from 'store/authSlice';
+import { RootState } from 'store/store';
 import Grades from '../grades';
 import { countryList } from '../signup/countries';
-import UsernameButton from '../signup/usernameButton';
+import UsernameButton2 from '../signup/usernameButton2';
 
 export default function OtherInfoStudent() {
+  const dispatch = useDispatch();
+  const { schoolName, username } = useSelector((state: RootState) => state.user.auth);
   return (
     <div>
           <h1 className='font-bold text-[32px]'>Create Your Account</h1>
             <label className='block text-xl font-semibold mt-6'>School Name</label>
-            <input type="text" className='block w-full h-[2.5rem] rounded-xl px-4 py-2 focus:outline-0 mt-3' placeholder='Enter School Name' required/>
+            <input type="text" className='block w-full h-[2.5rem] rounded-xl px-4 py-2 focus:outline-0 mt-3' placeholder='Enter School Name' required
+            value={schoolName}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              dispatch(updateUser({ key: 'schoolName', value: e.target.value }));
+            }}
+            />
 
             <label className='block text-xl font-semibold mt-6'>Select Country</label>
                 <select className='block w-full h-[2.5rem] rounded-xl px-4 py-2 focus:outline-0  mt-3'
                 onChange={(event: ChangeEvent<HTMLSelectElement>) => {
                   const value = event.target.options[event.target.selectedIndex].value;
-                  // dispatch(updateUser({ key: 'schoolCountry', value }));
-                  // dispatch(updateUser({ key: 'country', value }));
+                  dispatch(updateUser({ key: 'schoolCountry', value }));
+                  dispatch(updateUser({ key: 'country', value }));
                 }}
                 >   
                 <option value="Select School Country">Select School Country</option>
@@ -28,8 +38,12 @@ export default function OtherInfoStudent() {
                 })}
           </select>
           <label className='block text-xl font-semibold mt-6'>Username</label>
-          <input type="text" className='block w-full h-[2.5rem] rounded-xl px-4 py-2 focus:outline-0 mt-3' placeholder='Enter School Name' required/>
-          <UsernameButton />
+          <input type="text" value={username} className='block w-full h-[2.5rem] rounded-xl px-4 py-2 focus:outline-0 mt-3' placeholder='Enter School Name' required
+          onChange={(e) => {
+            dispatch(updateUser({ key: 'username', value: e.target.value }));
+          }}
+          />
+          <UsernameButton2 />
     </div>
   );
 }
