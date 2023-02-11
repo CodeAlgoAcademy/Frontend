d12843f6034a5ae282e4715290369bff3ce01c85
Olamide Simon <simonadepetoye@gmail.com>
2023-02-11 03:40:46 +0100

    Parent students

--- components/parentMultiForm/ParentSignUp6.tsx
+++ components/parentMultiForm/ParentSignUp6.tsx
@@ -13,7 +13,7 @@ export default function ParentSignUp5() {
       <label className="block text-xl font-semibold mt-6">Username</label>
       <input
         className="block w-full h-[2.5rem] rounded-xl px-4 py-2 focus:outline-0 mt-3"
-        type="email"
+        type="text"
         autoFocus
         required
       />
