51bb04fe72214381260d2107ca490f9c42a516d7
israeltheminer <israeliyanda5@gmail.com>
2023-02-12 08:54:56 +0100

    include conditional rendering of sidenav

--- pages/students.tsx
+++ pages/students.tsx
@@ -85,7 +85,7 @@ export default Index;
 
 const styles = {
   container:
-    'bg-[#ECEDF3] h-full px-5 md:px-20 py-5 overflow-x-auto min-h-screen flex-1 w-full students-container',
+    'bg-[#ECEDF3] h-full py-5 overflow-x-auto min-h-screen flex-1 w-full students-container',
   containerHeader:
     'flex justify-between py-3 items-center border-b border-b-slate-400 students-container',
   headerTitle: 'font-medium text-[30px] students-container text-[#2073fa]',
