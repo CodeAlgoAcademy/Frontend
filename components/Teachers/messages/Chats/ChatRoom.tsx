2376e20704c33d6562e778b84089403bcf68bb26
israeltheminer <israeliyanda5@gmail.com>
2023-02-17 20:11:22 +0100

    message page style fixes

--- components/Teachers/messages/Chats/ChatRoom.tsx
+++ components/Teachers/messages/Chats/ChatRoom.tsx
@@ -1,23 +1,23 @@
-import { Avatar, IconButton } from '@mui/material';
-import React, { FormEvent, useEffect, useState, ChangeEvent } from 'react';
-import { chats } from './chatsData';
-import { IMessage } from 'types/interfaces';
-import { GrAttachment } from 'react-icons/gr';
-import { BsEmojiSmile } from 'react-icons/bs';
-import { BiSend } from 'react-icons/bi';
-import { w3cwebsocket as W3CWebSocket } from 'websocket';
-import { getAccessToken } from 'utils/getTokens';
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from 'store/store';
+import {Avatar,IconButton} from '@mui/material';
+import React,{FormEvent,useEffect,useState,ChangeEvent} from 'react';
+import {chats} from './chatsData';
+import {IMessage} from 'types/interfaces';
+import {GrAttachment} from 'react-icons/gr';
+import {BsEmojiSmile} from 'react-icons/bs';
+import {BiSend} from 'react-icons/bi';
+import {w3cwebsocket as W3CWebSocket} from 'websocket';
+import {getAccessToken} from 'utils/getTokens';
+import {useSelector,useDispatch} from 'react-redux';
+import {RootState} from 'store/store';
 import {
   getConversations,
   getOpenMesssages,
   open_a_message,
   setOpenStudent,
 } from 'store/messagesSlice';
-import { openErrorModal } from 'store/fetchSlice';
+import {openErrorModal} from 'store/fetchSlice';
 import http from 'axios.config';
-import { User } from 'types/interfaces';
+import {User} from 'types/interfaces';
 
 interface Message {
   id: number;
@@ -30,11 +30,11 @@ export const client = new W3CWebSocket(
 );
 
 const ChatRoom = () => {
-  const { openedMessageOwner, openedMessage } = useSelector((state: RootState) => state.messages);
+  const {openedMessageOwner,openedMessage} = useSelector((state: RootState) => state.messages);
 
-  const { email } = useSelector((state: RootState) => state.user);
-  const [messages, setMessages] = useState<IMessage[]>(openedMessage ? [...openedMessage] : []);
-  const [typingText, setTypingText] = useState<string>('');
+  const {email} = useSelector((state: RootState) => state.user);
+  const [messages,setMessages] = useState<IMessage[]>(openedMessage ? [...openedMessage] : []);
+  const [typingText,setTypingText] = useState<string>('');
   const dispatch = useDispatch();
 
   // console.log(openedMessageOwner)
@@ -69,8 +69,8 @@ const ChatRoom = () => {
   };
 
   const handleSend: any = (e: ChangeEvent<HTMLInputElement>) => {
-    if (openedMessageOwner.id) {
-      if (typingText !== '') {
+    if(openedMessageOwner.id) {
+      if(typingText !== '') {
         e.preventDefault();
         client.send(
           JSON.stringify({
@@ -84,7 +84,7 @@ const ChatRoom = () => {
         setTypingText('');
       }
     } else {
-      dispatch(openErrorModal({ errorText: ['No user to send a message'] }));
+      dispatch(openErrorModal({errorText: ['No user to send a message']}));
     }
   };
 
@@ -95,21 +95,21 @@ const ChatRoom = () => {
 
     client.onmessage = (message: any) => {
       const dataFromServer = JSON.parse(message.data);
-      console.log('serverr reply', dataFromServer);
-      setMessages([...messages, dataFromServer]);
+      console.log('serverr reply',dataFromServer);
+      setMessages([...messages,dataFromServer]);
     };
-  }, []);
+  },[]);
 
   useEffect(() => {
     dispatch(getOpenMesssages());
-  }, [openedMessageOwner]);
+  },[openedMessageOwner]);
 
   // useEffect(() => {
   //   openedMessage && setMessages([...openedMessage]);
   // }, [openedMessage]);
 
   return (
-    <div className="max-h-[35rem] h-[75vh]">
+    <div className="h-full relative">
       <div className={styles.header}>
         <Avatar src="" alt="" />
         <p className="font-bold text-sm capitalize">
@@ -118,13 +118,12 @@ const ChatRoom = () => {
       </div>
       <div className={styles.chatContainer}>
         {[...messages]
-          ?.sort((a: any, b: any) => a.date - b.date)
-          ?.map((chat, index) => (
+          ?.sort((a: any,b: any) => a.date - b.date)
+          ?.map((chat,index) => (
             <div key={index}>
               <p
-                className={`${styles.chats} ${
-                  chat.user.email === email ? 'ml-auto rounded-bl-lg' : 'rounded-br-lg'
-                }`}
+                className={`${styles.chats} ${chat.user.email === email ? 'ml-auto rounded-bl-lg' : 'rounded-br-lg'
+                  }`}
               >
                 {chat.text}
               </p>
@@ -164,9 +163,9 @@ export default ChatRoom;
 
 const styles = {
   header: 'flex items-center space-x-2 border-b-2 p-2',
-  chatContainer: 'h-[100%] space-y-1 p-5 px-5 overflow-y-auto',
+  chatContainer: 'space-y-1 p-5 px-5 overflow-y-auto max-h-[100%]',
   chats: 'bg-[#fff3cc] p-3 text-xs w-fit rounded-t-lg',
-  messageInputContainer: 'bg-white w-full rounded-b-xl',
+  messageInputContainer: 'bg-white w-full border-t p-2 rounded-b-xl absolute left-0 bottom-0',
   inputContainer: 'flex items-center space-x-2 text-slate-400',
   input: 'w-full p-1 outline-none bg-transparent text-black',
 };
