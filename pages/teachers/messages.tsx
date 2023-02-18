5fd99c996caf3badd90e227661f12ba319e9f012
israeltheminer <israeliyanda5@gmail.com>
2023-02-18 14:13:41 +0100

    teacher's message page UI and style changes

--- pages/teachers/messages.tsx
+++ pages/teachers/messages.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Index = () => {
   return (
-    <TeacherLayout className='md:pl-0 md:pr-0 pt-0 pb-0 pl-0 pr-0 border overflow-auto'>
+    <TeacherLayout className='md:pl-0 md:pr-0 pt-0 pb-0 pl-0 pr-0 border overflow-hidden relative'>
       <Messages />
     </TeacherLayout>
   );
