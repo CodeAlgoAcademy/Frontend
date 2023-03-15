eabcf88f31cfb9bffd848f031efdd7ee0b862546
daniel-dunsin <adejaredaniel12@gmail.com>
2023-03-15 14:32:46 +0100

    add child multiplayer

--- components/parents/AddAFriend.tsx
+++ components/parents/AddAFriend.tsx
@@ -1,31 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addChildFriend, updateChild } from "store/parentChildSlice";
+import { RootState } from "store/store";
 
 const AddAFriend = () => {
-  const [detail, setDetail] = useState<string>('');
-  const updateDetail = (value: string) => {
-    setDetail(value);
-  };
-  return (
-    <section className="small-scroll-thumb blue-scroll-thumb">
-      <p className="mt-14 text-sm font-light mb-4">
-        CodeAlgo allows parents to limit students multiplayer interactions. Please enter the email
-        or username of your student{"'"}s friend below. A request will be sent to the linked parent
-        account. They can accept and decline the request
-      </p>
-      <h2 className="font-bold text-[15px]">Friend email or username</h2>
-      <input
-        type="text"
-        className="w-full max-w-[400px] px-3 py-1 rounded-xl border-2 focus:border-[#2073FA] placeholder:text-gray-500 outline-none text-[15px] mb-5"
-        placeholder="hank45"
-      />
-      <button className="bg-[#2073fa] rounded-xl py-1 px-3 w-full max-w-[400px] font-light text-white mb-4">
-        Send Request
-      </button>
-      <p className="text-[14px]">
-        Requests will be accepted or denied by the student{"'"}s parent account
-      </p>
-    </section>
-  );
+   const [detail, setDetail] = useState<string>("");
+   const updateDetail = (value: string) => {
+      setDetail(value);
+   };
+
+   const { friend, currentChild } = useSelector((state: RootState) => state.parentChild);
+   const dispatch = useDispatch();
+   return (
+      <section className="small-scroll-thumb blue-scroll-thumb">
+         <p className="mt-14 mb-4 text-sm font-light">
+            CodeAlgo allows parents to limit students multiplayer interactions. Please enter the email or username of your student{"'"}s friend below.
+            A request will be sent to the linked parent account. They can accept and decline the request
+         </p>
+         <h2 className="text-[15px] font-bold">Friend email or username</h2>
+         <input
+            type="text"
+            className="mb-5 w-full max-w-[400px] rounded-xl border-2 px-3 py-1 text-[15px] outline-none placeholder:text-gray-500 focus:border-[#2073FA]"
+            placeholder="hank45"
+            value={friend}
+            onChange={(e) => {
+               dispatch(updateChild({ key: "friend", value: e.target.value }));
+            }}
+         />
+         <button
+            onClick={() => {
+               if ((friend?.length as number) > 0) {
+                  dispatch(addChildFriend(currentChild.username));
+               }
+            }}
+            className="mb-4 w-full max-w-[400px] rounded-xl bg-[#2073fa] py-1 px-3 font-light text-white"
+         >
+            Send Request
+         </button>
+         <p className="text-[14px]">Requests will be accepted or denied by the student{"'"}s parent account</p>
+      </section>
+   );
 };
 
 export default AddAFriend;
