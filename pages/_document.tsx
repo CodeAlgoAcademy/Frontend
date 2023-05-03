37bc9b198ae74b2ddc167d918e41263a00ee30fc
daniel-dunsin <adejaredaniel12@gmail.com>
2023-05-03 22:14:27 +0100

    fixing tests

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,10 +10,31 @@ class MyDocument extends Document {
    render() {
       return (
          <Html>
-            <Head></Head>
+            <Head>
+               <title>CodeAlgo Academy</title>
+               <meta name="description" content="CodeAlgo Academy" />
+               <meta property="og:title" content="CodeAlgo Academy" />
+               <meta property="twitter:title" content="CodeAlgo Academy" />
+               <meta
+                  property="og:description"
+                  content="3D games from CodeAlgo academy will bring students to computer sciences where they can teach themselves as they play.CodeAlgo Academy"
+               />
+               <meta
+                  property="twitter:description"
+                  content="3D games from CodeAlgo academy will bring students to computer sciences where they can teach themselves as they play.CodeAlgo Academy"
+               />
+               <meta property="image" content="https://gcdnb.pbrd.co/images/9lHbIun6obws.jpg?o=1" />
+
+               <meta property="og:image" content="https://gcdnb.pbrd.co/images/9lHbIun6obws.jpg?o=1" />
+               <meta property="twitter:image" content="https://gcdnb.pbrd.co/images/9lHbIun6obws.jpg?o=1" />
+               <meta property="image" content="https://gcdnb.pbrd.co/images/9lHbIun6obws.jpg?o=1" />
+            </Head>
             <body>
                <Main />
                <NextScript />
+
+               <script async src="https://www.googletagmanager.com/gtag/js?id=G-BT6M4N6SS1"></script>
+               <script async src="../utils/analytics.js"></script>
             </body>
          </Html>
       );
--- tests/pages/parents/screentime.spec.tsx
+++ tests/pages/parents/screentime.spec.tsx
@@ -1,12 +1,21 @@
+import Layout from "@/components/Layout";
 import ScreenTime from "@/pages/parents/screen-time";
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "store/store";
+import { getAccessToken } from "utils/getTokens";
+
+const myMock = jest.fn();
+
+jest.mock("getAccessToken");
+(getAccessToken as any).mockImplementation(() => Promise.resolve());
 
 it("Should render two containers", () => {
    render(
       <Provider store={store}>
-         <ScreenTime />
+         <Layout>
+            <ScreenTime />
+         </Layout>
       </Provider>
    );
 
