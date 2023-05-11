8a288374531a0e776cadeee11d08b79351696491
daniel-dunsin <adejaredaniel12@gmail.com>
2023-05-11 06:31:47 +0100

    merged types

--- components/Teachers/messages/ChatTabs.tsx
+++ components/Teachers/messages/ChatTabs.tsx
@@ -9,7 +9,7 @@ import { CurrentClassState } from "types/interfaces";
 
 const ChatTabs = () => {
    const [active, setActive] = useState(0);
-   const { students } = useSelector((state: RootState) => state.students.students);
+   const students = useSelector((state: RootState) => state.students.students);
    const { teachers } = useSelector((state: RootState) => state.allTeachers);
 
    const currentClass = useSelector((state: RootState): CurrentClassState => state.currentClass);
@@ -34,7 +34,7 @@ const ChatTabs = () => {
          <article className={openedStudent ? "max-h-full flex-1 overflow-y-auto bg-gray-100 p-0" : "overflow-y-auto bg-gray-100"}>
             {openedStudent && (
                <div className="overflow-hidden">
-                  {students?.map((student: any) => {
+                  {students?.students?.map((student: any) => {
                      return (
                         <p
                            onClick={() => {
