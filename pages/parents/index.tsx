2a8ef77d392b41f50021708f32a81f06ade1d67c
Olamide Simon <simonadepetoye@gmail.com>
2023-05-18 17:16:37 +0100

    fixes

--- pages/parents/index.tsx
+++ pages/parents/index.tsx
@@ -5,19 +5,27 @@ import ParentLayout from "@/components/layouts/ParentLayout";
 import ProgressBar from "@/components/parents/UI/ProgressBar";
 import SkillBox from "@/components/parents/student/SkillBox";
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getChildren } from "store/parentChildSlice";
+import { RootState } from "store/store";
 
 const Dashboard = () => {
+   const dispatch = useDispatch();
+   const { currentChild } = useSelector((state: RootState) => state.parentChild);
+
+   useEffect(() => {
+      dispatch(getChildren());
+   }, []);
+
    return (
       <ParentLayout>
          <div className="relative bottom-14 mb-[-120px] scale-90 overflow-x-auto sm:bottom-0 sm:mb-0 sm:scale-100">
             <div className="mx-auto mb-6 flex flex-wrap items-start justify-center gap-x-6 gap-y-8">
                <ContentBox size="base" title="Level" padding="small">
                   <h2 className="mt-14 text-center text-[22px] font-medium">Level 11</h2>
-                  <p className="mt-2 text-center text-sm font-light">Variables & Syntax</p>
+                  {currentChild && <p className="mt-2 text-center text-sm font-light">{currentChild?.progress[0]?.unit}</p>}
                   <div className="mt-6 ml-4">
-                     <ProgressBar color="red" percentage={65} title="Progress" titleSize="base" />
+                     {currentChild && <ProgressBar color="red" percentage={currentChild?.progress[0]?.lesson} title="Progress" titleSize="base" />}
                      <div className="mt-8">
                         <h3 className="font-semibold">Comprehension Tracking</h3>
                         <div className="small-scroll-thumb blue-scroll-thumb mt-3 flex h-[70px] flex-col gap-5 overflow-y-auto pr-4">
