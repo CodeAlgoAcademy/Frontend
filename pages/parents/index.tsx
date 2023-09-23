5488557f99ef4667650a45b9496216a0908cc90b
Olamide <simonadepetoye@gmail.com>
2023-09-23 16:09:29 +0100

    --empty

--- pages/parents/index.tsx
+++ pages/parents/index.tsx
@@ -38,7 +38,7 @@ const Dashboard = () => {
 
    useEffect(() => {
       dispatch(getChildren());
-      dispatch(getChildProgress())
+      dispatch(getChildProgress());
    }, []);
 
    useEffect(() => {
@@ -47,7 +47,7 @@ const Dashboard = () => {
       }
    }, [parent?.currentChild, parent?.currentChild?.timeLimits]);
 
-   console.log(parent?.currentChild?.progress)
+   console.log(parent?.currentChild?.progress);
    return (
       <ParentLayout>
          <div className="relative bottom-14 mb-[-120px] scale-90 overflow-x-auto sm:bottom-0 sm:mb-0 sm:scale-100">
@@ -55,20 +55,25 @@ const Dashboard = () => {
                <ContentBox size="base" title="Level" padding="small">
                   <h2 className="mt-14 text-center text-[22px] font-medium"></h2>
                   {/* {parent?.currentChild?.progress?.map((progress, index) => ( */}
-                     <React.Fragment>
-                        <p className="mt-2 text-center text-sm font-light">{parent?.currentChild?.progress?.title || ""}</p>
-                        <div className="mt-6 ml-4">
-                           <ProgressBar color="red" percentage={parent?.currentChild?.progress?.progress || 0} title="Progress" titleSize="base" />
-                           <div className="mt-8">
-                              <h3 className="font-semibold">Comprehension Tracking</h3>
-                              {/* <div className="small-scroll-thumb blue-scroll-thumb mt-3 flex h-[70px] flex-col gap-5 overflow-y-auto pr-4">
+                  <React.Fragment>
+                     <p className="mt-2 text-center text-sm font-light">{parent?.currentChild?.progress?.title || ""}</p>
+                     <div className="mt-6 ml-4">
+                        <ProgressBar
+                           color="red"
+                           percentage={Number(parent?.currentChild?.progress?.progress) || 0}
+                           title="Progress"
+                           titleSize="base"
+                        />
+                        <div className="mt-8">
+                           <h3 className="font-semibold">Comprehension Tracking</h3>
+                           {/* <div className="small-scroll-thumb blue-scroll-thumb mt-3 flex h-[70px] flex-col gap-5 overflow-y-auto pr-4">
                                  {progress.assignment?.map(({ completed, title }) => (
                                     <ProgressBar color="green" percentage={completed} title={title} titleSize="small" />
                                  ))}
                               </div> */}
-                           </div>
                         </div>
-                     </React.Fragment>
+                     </div>
+                  </React.Fragment>
                   {/* ))} */}
                </ContentBox>
                <ContentBox size="base" title="Skills" padding="small">
