78eb09f0a02801814d1c4365d279e3b0700ad2f3
daniel-dunsin <adejaredaniel12@gmail.com>
2023-12-04 15:57:11 +0100

    beta announcement tab

---
+++ components/UI/announcements.tsx
@@ -0,0 +1,29 @@
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { IoInformationCircle } from "react-icons/io5";
+import { MdCancel, MdClose } from "react-icons/md";
+import { AnnouncementStates, getAnnouncementSession, removeAnnouncementSession } from "utils/announcement";
+
+export default function Announcements() {
+   const slug = useRouter();
+   const [isOpen, setIsOpen] = useState(true);
+
+   //    it should have teacher or parent in the route but it shouldn't be /login/teacher or /signup/teacher
+   const isDashboardPage =
+      (slug.pathname.includes("/teacher") || slug.pathname.includes("parent")) &&
+      !(slug.pathname.includes("login") || slug.pathname.includes("signup"));
+
+   if (!isOpen) return <></>;
+
+   return (
+      <div className={`text-white ${isDashboardPage ? "bg-[#2073fa]" : "bg-orange-400"}  z-[50] flex w-full items-center justify-between p-2`}>
+         <div></div>
+         <p className="flex cursor-pointer items-center gap-2">
+            <IoInformationCircle /> This is a beta
+         </p>
+         <span>
+            <MdClose cursor="pointer" size={26} onClick={() => setIsOpen(false)} />
+         </span>
+      </div>
+   );
+}
