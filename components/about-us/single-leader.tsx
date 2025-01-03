40f59650170f50e7e1e7bf745df12b2336d232cb
daniel-dunsin <adejaredaniel12@gmail.com>
2025-01-03 17:41:41 +0100

    fix: ui fixes

--- components/about-us/single-leader.tsx
+++ components/about-us/single-leader.tsx
@@ -7,11 +7,11 @@ export default function SingleLeader({ name, info, img, position }: Props) {
    return (
       <article>
          <div className="mx-auto h-[250px] w-[250px]">
-            <img src={img} className="h-full w-full rounded-md object-cover shadow-md hover:shadow-lg" alt="" />
+            <img src={img} className="h-full w-full rounded-md object-contain" alt="" />
          </div>
          <h1 className="mt-4 text-center font-thabit text-[1.8rem] font-bold leading-tight text-mainPink max-md:text-[1.5rem]">{name}</h1>
          <p className="mt-1 text-center font-thabit text-[1.2rem] font-bold max-md:text-[1.1rem]">{position}</p>
-         <p className="mt-1 text-center font-thabit text-[1rem] font-bold leading-tight max-md:text-[.9rem]">{info}</p>
+         <p className="mt-1 text-center font-thabit text-[1rem] leading-tight max-md:text-[.9rem]">{info}</p>
       </article>
    );
 }
