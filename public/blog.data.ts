3503fca88b39eaed137ce8d93fa20ec9c95ced18
daniel-dunsin <adejaredaniel12@gmail.com>
2025-01-03 17:49:31 +0100

    fix: ui fixes

--- public/blog.data.ts
+++ public/blog.data.ts
@@ -18,7 +18,7 @@ export const posts: Post[] = [
       title: "Code and Play",
       body: "The Power of Gamification at CodeAlgo Academy",
       date: "December 2023",
-      image: "/assets/blog/learining.jpg",
+      image: "/assets/blog/learining1.jpg",
       detailPage: "/code-and-play",
    },
    {
@@ -39,7 +39,7 @@ export const posts: Post[] = [
       title: "Nurturing Youngsters",
       body: "The Guardian’s Guide to Nurturing Coding Skills in Kids",
       date: "December 2023",
-      image: "/assets/blog/guidance.jpg",
+      image: "/assets/blog/guidance1.jpg",
       detailPage: "/guidance-in-coding",
    },
 ];
