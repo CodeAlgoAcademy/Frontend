90cec4abd5282b7e9457c8e26103dd9c3961a6dc
CodeAlgoAcademy <107329691+CodeAlgoAcademy@users.noreply.github.com>
2025-10-04 23:37:20 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -39,14 +39,22 @@ function MyApp({ Component, pageProps }: AppProps) {
 
     
    return (
-        <script async src="https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}"></script>
-        <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', '${GA_MEASUREMENT_ID}');
-        </script>
+        <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        />
+        <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `,
+        }} 
+        />
            
       <Provider store={store}>
          <Layout>
