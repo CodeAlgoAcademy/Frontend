670879ba961ec4c1231de228c59cde3198569cb0
ukponotitus <98172271+ukponotitus@users.noreply.github.com>
2025-12-24 10:48:19 +0100

    Feature/revamp landing page (#58)
    * Triumfia added google site verification to  pages/index.tsx
    
    * Triumfia added site verification to pages/index.tsx
    
    * Triumfia added Google tag to enable google analytics on pages/_app.tsx
    
    * Triumfia adding gtag_app.tsx
    
    * Update _app.tsx
    
    * Triumfia reverted google analytics change pages/_app.tsx
    
    * feat(landingPage):revamping
    
    * feat: revamp site UI and introduce sales banners and pricing updates
    
    * fix responsiveness
    
    ---------
    
    Co-authored-by: CodeAlgoAcademy <107329691+CodeAlgoAcademy@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,30 @@
 import type { NextPage } from "next";
 import React from "react";
-import Navbar from "@/components/navbar/home/Navbar";
+import Head from "next/head"; 
 import Hero from "@/components/home/new-home/hero";
 import WhatWeBuilt from "@/components/home/new-home/what-we-built";
-import LetsLearnTogether from "@/components/home/new-home/lets-learn-together";
 import VoiceOfOurCommunity from "@/components/home/new-home/voice-of-our-community";
 import GetStarted from "@/components/home/new-home/get-started";
 import CodeToSuccess from "@/components/home/new-home/code-to-success";
 import AsSeenIs from "@/components/home/new-home/as-seen-in";
 import Footer from "@/components/home/new-home/footer";
-import AccessibilityMenu from "@/components/navbar/home/accessibility/menu";
+import {WinterBanner} from "@/components/home/new-home/winter-banner";
+import ValuePropositions from "@/components/home/new-home/ValuePropositions";
+import Navbar from "@/components/navbar/home/Navbar";
 
 const Home: NextPage = () => {
    return (
       <div className="relative  bg-white" suppressHydrationWarning>
+           <Head>
+            <meta
+               name="google-site-verification"
+               content="eGB3Olxnsy0kXPD_3EoaI1Fzl7xsQVK4R1WxbBSCrFI"
+            />
+         </Head>
          <Navbar />
+         <WinterBanner />
          <Hero />
+         <ValuePropositions /> 
          <WhatWeBuilt />
          <GetStarted />
          <CodeToSuccess />
