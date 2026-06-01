fcbaa04cd20838d2f6f9fda01121bcdc95284e69
ukponotitus <titusukpono0@gmail.com>
2026-06-01 14:17:51 +0100

    add other color to callroom palette

--- components/Teachers/addClass/colors.ts
+++ components/Teachers/addClass/colors.ts
@@ -1,1 +1,1 @@
-export const colors: Array<string> = ['#F6B86F', '#AADE98', '#92C7F7', '#FFE977'];
+export const colors: Array<string> = ['#F6B86F', '#AADE98', '#92C7F7', '#FFE977', '#800080', '#FF0000'];
